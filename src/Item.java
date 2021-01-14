249de005d2b7e81437c36dd66f9f6a6d525ba5e3
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2021-01-14 14:35:28 -0800

    create project

---
+++ src/Item.java
@@ -0,0 +1,9 @@
+public class Item {
+  public boolean solved;
+  public String description;
+
+  public Item(String description, boolean solved) {
+    this.solved = solved;
+    this.description = description;
+  }
+}
